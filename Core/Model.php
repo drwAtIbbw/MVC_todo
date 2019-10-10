f3dd5d36407f111f9cdfd4102e8a7feee7dc9deb
drwAtIbbw <48730533+drwAtIbbw@users.noreply.github.com>
2019-10-10 15:49:55 +0200

    Move basic methods to parent class Model.php

--- Core/Model.php
+++ Core/Model.php
@@ -1,6 +1,18 @@
 <?php
     class Model
     {
-
+        protected $tablename;
+        protected $colnames = array();
+        
+        /* insert CRUD functions here leave only special functions in Model/Tasks.php */
+        
+        public function showAll() //index method
+        {
+         $sql = "SELECT * FROM ".$this->tablename;
+            $req = Database::getBdd()->prepare($sql);
+            $req->execute();
+            
+        return $req->fetchAll();
+        }
     }
 ?>
