ce301a397a88d551d7f501fa560cbe3f50b2c797
drwAtIbbw <48730533+drwAtIbbw@users.noreply.github.com>
2019-10-13 10:49:07 +0200

    model contains all basic db actions

--- Core/Model.php
+++ Core/Model.php
@@ -1,18 +1,73 @@
 <?php
     class Model
     {
-        protected $tablename;
-        protected $colnames = array();
-        
-        /* insert CRUD functions here leave only special functions in Model/Tasks.php */
-        
-        public function showAll() //index method
-        {
-         $sql = "SELECT * FROM ".$this->tablename;
-            $req = Database::getBdd()->prepare($sql);
-            $req->execute();
-            
+	protected $tablename;
+	protected $colnames = [];
+	
+	public function create($werte)
+    {
+	$cols = str_replace('id,','',$this->colnames[0]);
+	// Build query****
+	$sql = "INSERT INTO ".$this->tablename." (".$cols.") VALUES (";
+	$col_namen = explode(',',$cols);
+
+	for ($i=0;$i<count($col_namen);$i++) {
+		$col_namen[$i]= ":".$col_namen[$i];
+	}
+	$cols_pdo = join(',',$col_namen);
+	$sql.=$cols_pdo.");\"";       
+	//*******
+        $req = Database::getBdd()->prepare($sql);
+        return $req->execute($werte);
+    }
+	public function edit($id, $werte)
+    {
+	$cols = str_replace('id,','',$this->colnames[0]);        
+	//prepare query ********
+	$sql = "UPDATE ".$this->tablename." SET ";
+	$col_namen = explode(',',$cols);
+
+	for ($i=0;$i<count($col_namen);$i++) {
+		$col_namen[$i]= $col_namen[$i]." =:".$col_namen[$i];
+	}
+	$cols_pdo = join(',',$col_namen);
+	$sql.=$cols_pdo." WHERE id =:id;\"";  
+	//********	
+	
+	$werte['id'] = $id;
+	
+	
+        $req = Database::getBdd()->prepare($sql);
+        return $req->execute($werte);
+    }
+	 public function showAll()
+    	{
+        $sql = "SELECT * FROM ".$this->tablename;
+        $req = Database::getBdd()->prepare($sql);
+        $req->execute();
+	
+	
         return $req->fetchAll();
-        }
+    	}
+	public function show($id)
+    	{
+        $sql = "SELECT * FROM ".$this->tablename." WHERE id =" . $id;
+        $req = Database::getBdd()->prepare($sql);
+        $req->execute();
+        return $req->fetch();
+    	}
+	public function delete($id)
+    	{
+	     
+	$sql = "DELETE FROM ".$this->tablename." WHERE id = :id";
+        $req = Database::getBdd()->prepare($sql);
+	/*try {
+         $req->execute([':id' => $id]);
+	}catch (PDOException $e) {
+   	 echo $e->getMessage();
+  	}*/
+
+	return $req->execute([':id' => $id]);
+    	}
     }
 ?>
