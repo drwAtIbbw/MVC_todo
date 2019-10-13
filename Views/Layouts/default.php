77891b5262538f5c787ed09b18da8cf344e846bf
drwAtIbbw <48730533+drwAtIbbw@users.noreply.github.com>
2019-10-13 10:59:25 +0200

    minor configuration changes

--- Views/Layouts/default.php
+++ Views/Layouts/default.php
@@ -2,12 +2,12 @@
 <head>
     <meta charset="utf-8">
 
-    <title>MVC Todo</title>
+    <title>sqlite</title>
 
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet">
 
     <link href="starter-template.css" rel="stylesheet">
-
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
     <style>
         body {
             padding-top: 5rem;
@@ -21,7 +21,7 @@
 
 <body>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="#">MVC Todo</a>
+    <a class="navbar-brand" href="<?= WEBROOT ?>users">SQLITE</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -29,9 +29,24 @@
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="<?= WEBROOT ?>users">Home<span class="sr-only">(current)</span></a>
+		
             </li>
+		<li class="nav-item">
+                <a class="nav-link" href="<?= WEBROOT ?>schuelers">Schüler</a>
+		
+            </li>
+        	</li>
+		<li class="nav-item">
+                <a class="nav-link" href="<?= WEBROOT ?>users/logout">Logout</a>
+		
+            </li>
+            
         </ul>
+	<form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
     </div>
 </nav>
 
@@ -40,6 +55,7 @@
     <div class="starter-template">
 
         <?php
+	
         echo $content_for_layout;
         ?>
 
@@ -47,6 +63,6 @@
 
 </main>
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+
 </body>
 </html>
