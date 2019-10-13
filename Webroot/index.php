0a49e4f29c02635ee6d34ded15e42366b76570ab
drwAtIbbw <48730533+drwAtIbbw@users.noreply.github.com>
2019-10-13 11:01:18 +0200

    added session

--- Webroot/index.php
+++ Webroot/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 define('WEBROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_NAME"]));
 define('ROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
 
