1236fc86aa7680cf09c88e7240fc539dda44eb11
drwAtIbbw <48730533+drwAtIbbw@users.noreply.github.com>
2019-10-13 11:04:29 +0200

    adjusted to accomodate whatever name of webroot

--- router.php
+++ router.php
@@ -1,15 +1,12 @@
 <?php
-
 class Router
 {
-
     static public function parse($url, $request)
     {
         $url = trim($url);
-
-        if ($url == "/PHP_Rush_MVC/")
+        if ($url == WEBROOT)
         {
-            $request->controller = "tasks";
+            $request->controller = "users";
             $request->action = "index";
             $request->params = [];
         }
@@ -17,11 +14,14 @@ class Router
         {
             $explode_url = explode('/', $url);
             $explode_url = array_slice($explode_url, 2);
+	
             $request->controller = $explode_url[0];
-            $request->action = $explode_url[1];
+		if (!isset($explode_url[1])) {
+			$request->action = 'index';
+		} else {
+            	$request->action = $explode_url[1];}
             $request->params = array_slice($explode_url, 2);
         }
-
     }
 }
 ?>
