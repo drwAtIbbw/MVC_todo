55575b4d84ecf649df6c8a7ffe5650c6fdee823d
drwAtIbbw <48730533+drwAtIbbw@users.noreply.github.com>
2019-10-13 10:46:50 +0200

    implement: secure_form, action-dependent-authen.

--- Core/Controller.php
+++ Core/Controller.php
@@ -1,47 +1,67 @@
 <?php
     class Controller
     {
-        var $vars = [];
-        var $layout = "default";
+        public $vars = [];
+        public $layout = "default";
+	protected $controller;
+
+	function __construct() 
+	{     
+	
+	require_once(ROOT . 'router.php');
+	require_once(ROOT . 'request.php');
+	
+	$this->request = new Request();
+	Router::parse($this->request->url,$this->request);
+	
+	}
+
 
         function set($d)
         {
             $this->vars = array_merge($this->vars, $d);
         }
-
         function render($filename)
         {
-            extract($this->vars);
+		       
+		extract($this->vars);
             ob_start();
             require(ROOT . "Views/" . ucfirst(str_replace('Controller', '', get_class($this))) . '/' . $filename . '.php');
             $content_for_layout = ob_get_clean();
-
             if ($this->layout == false)
             {
-                $content_for_layout;
+                echo($content_for_layout);
             }
             else
             {
                 require(ROOT . "Views/Layouts/" . $this->layout . '.php');
+		  
             }
         }
-
+	public function isauthenticated($action) 
+	{
+		if ($action ==='login' || $action === 'logout') {
+			return true;
+		} 
+		if(!isset($_SESSION['user'])) {
+		return false;
+		}
+		return true; 
+	}
         private function secure_input($data)
         {
-            $data = trim($data);
+           $data = trim($data);
             $data = stripslashes($data);
             $data = htmlspecialchars($data);
             return $data;
         }
-
         protected function secure_form($form)
         {
             foreach ($form as $key => $value)
             {
                 $form[$key] = $this->secure_input($value);
             }
-            return $form;
+		return $form;
         }
-
     }
 ?>
