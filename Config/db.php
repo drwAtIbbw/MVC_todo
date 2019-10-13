8e8a088a0a7f1e51c838354c2198ebaa3c26b104
drwAtIbbw <48730533+drwAtIbbw@users.noreply.github.com>
2019-10-13 10:41:49 +0200

    added close function
    not really necessary, because connection is not persistent but in case...

--- Config/db.php
+++ Config/db.php
@@ -1,17 +1,22 @@
 <?php
-
 class Database
 {
     private static $bdd = null;
-
     private function __construct() {
     }
-
     public static function getBdd() {
         if(is_null(self::$bdd)) {
-            self::$bdd = new PDO("mysql:host=localhost;dbname=todo_php", 'root', 'root');
+            self::$bdd = new PDO("sqlite:".ROOT."schule.db");
+		self::$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return self::$bdd;
+    }
+	public static function closeBdd() {
+        if(!is_null(self::$bdd)) {
+            self::$bdd = null;
+		return true;
+        }
+        return false;
     }
 }
 ?>
